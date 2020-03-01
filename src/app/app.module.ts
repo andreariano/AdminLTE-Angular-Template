dcfe7d5df0ec045906ebb0c0c3d96ebfa20247cc
Andre Ariano <andreariano@yahoo.com.br>
2020-03-01 12:19:20 -0300

    Admin LTE Angular template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppheaderComponent } from './appheader/appheader.component';
+import { AppmenuComponent } from './appmenu/appmenu.component';
+import { AppfooterComponent } from './appfooter/appfooter.component';
+import { AppsettingComponent } from './appsetting/appsetting.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppheaderComponent,
+    AppmenuComponent,
+    AppfooterComponent,
+    AppsettingComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
